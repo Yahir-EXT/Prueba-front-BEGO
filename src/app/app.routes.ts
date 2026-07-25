f0973f4cd64624c2408018297e247bbd5242cfea
Yahir-EXT <lalo.xochilt@gmail.com>
2026-07-24 18:55:45 -0600

    feat: configura routing para pantalla de detalle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { OrdersList } from './components/orders-list/orders-list';
+import { OrderDetail } from './components/order-detail/order-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: OrdersList },
+    { path: 'order/:id', component: OrderDetail }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { OrdersList } from './components/orders-list/orders-list';
 
 @Component({
   selector: 'app-root',
-  imports: [OrdersList],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
